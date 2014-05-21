b0507527ce81dcaf1fe3598fa7736885baf1442b
tankal1110 <tanka1110girl@mail.ru>
2014-05-21 16:37:33 +0300

    m

--- FunnyPuzzle/View Controllers/GamePlay/GamePlayViewController.m
+++ FunnyPuzzle/View Controllers/GamePlay/GamePlayViewController.m
@@ -8,7 +8,13 @@
 
 #import "GamePlayViewController.h"
 #import "GameObject.h"
+#import <SVGKit/SVGKFastImageView.h>
+#import "Segment.h"
+
 @interface GamePlayViewController ()
+{
+  NSArray *images;
+}
 @property (nonatomic, weak) IBOutlet UIView *leftView;
 @property (nonatomic, weak) IBOutlet UIView *rightView;
 @property (nonatomic, weak) IBOutlet UIView *centerView;
@@ -20,6 +26,7 @@
 - (IBAction)next:(id)sender;
 - (IBAction)prew:(id)sender;
 - (IBAction)back:(id)sender;
+
 
 
 @end
@@ -40,9 +47,28 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     //FPGameType *gt=[GameObject sharedInstance].gameType;
+    
+    SVGKImage *im1=[SVGKImage imageNamed:@"pencil1.svg"];
+    SVGKImage *im2=[SVGKImage imageNamed:@"pencil2.svg"];
+    Segment *s1 = [Segment new];
+    Segment *s2 = [Segment new];
+    s1.rect=CGRectMake(10, 15, 50, 50);
+    s2.rect=CGRectMake(10, 10, 50, 50);
+    s1.image = im1;
+    s2.image = im2;
+    [self.view addSubview:s1];
+    [self.view addSubview:s2];
+   // SVGKImage *im3=[SVGKImage imageNamed:@"pencil_gray.svg"];
+  //  SVGKImage *im4=[SVGKImage imageNamed:@"pencil.svg"];
+    
+    
+    
+    images=[NSArray arrayWithObjects:im1,im2, nil];
+  
     FPGameType gt=FPGameModeFirs;
     switch (gt) {
         case FPGameModeFirs:
+            
             NSLog(@"Hello1 x  %f   y  %f", self.rightView.frame.origin.x, self.rightView.frame.origin.y);
             
             break;
