0ff1657bb7fd34936a830fe373732885d0f53acc
Stas Volskyi <Admin@Admins-Mac.local>
2014-05-22 21:26:52 +0300

    add Preferences

---
+++ FunnyPuzzle/View Controllers/Preferenсes/Preferences.h
@@ -0,0 +1,13 @@
+//
+//  Preferencces.h
+//  FunnyPuzzle
+//
+//  Created by Misha on 21.05.14.
+//  Copyright (c) 2014 Mobilez365. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Preferences : UIViewController
+
+@end
